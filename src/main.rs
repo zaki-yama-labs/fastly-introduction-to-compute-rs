d2a80628a0460b30e323f4e0006cdae88750fdbf
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2025-08-03 21:41:56 +0900

    3 > Adding the origin hostname and overriding the host header

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 //! Default Compute template program.
 
-use fastly::http::{header, Method, StatusCode};
-use fastly::{mime, Error, Request, Response};
+use fastly::{Error, Request, Response};
 
 /// The entry point for your application.
 ///
@@ -12,62 +11,6 @@ use fastly::{mime, Error, Request, Response};
 /// If `main` returns an error, a 500 error response will be delivered to the client.
 #[fastly::main]
 fn main(req: Request) -> Result<Response, Error> {
-    // Log service version
-    println!(
-        "FASTLY_SERVICE_VERSION: {}",
-        std::env::var("FASTLY_SERVICE_VERSION").unwrap_or_else(|_| String::new())
-    );
-
-    // Filter request methods...
-    match req.get_method() {
-        // Block requests with unexpected methods
-        &Method::POST | &Method::PUT | &Method::PATCH | &Method::DELETE => {
-            return Ok(Response::from_status(StatusCode::METHOD_NOT_ALLOWED)
-                .with_header(header::ALLOW, "GET, HEAD, PURGE")
-                .with_body_text_plain("This method is not allowed\n"))
-        }
-
-        // Let any other requests through
-        _ => (),
-    };
-
-    // Pattern match on the path...
-    match req.get_path() {
-        // If request is to the `/` path...
-        "/" => {
-            // Below are some common patterns for Compute services using Rust.
-            // Head to https://developer.fastly.com/learning/compute/rust/ to discover more.
-
-            // Create a new request.
-            // let mut bereq = Request::get("http://httpbin.org/headers")
-            //     .with_header("X-Custom-Header", "Welcome to Compute!")
-            //     .with_ttl(60);
-
-            // Add request headers.
-            // bereq.set_header(
-            //     "X-Another-Custom-Header",
-            //     "Recommended reading: https://developer.fastly.com/learning/compute",
-            // );
-
-            // Forward the request to a backend.
-            // let mut beresp = bereq.send("backend_name")?;
-
-            // Remove response headers.
-            // beresp.remove_header("X-Another-Custom-Header");
-
-            // Log to a Fastly endpoint.
-            // use std::io::Write;
-            // let mut endpoint = fastly::log::Endpoint::from_name("my_endpoint");
-            // writeln!(endpoint, "Hello from the edge!").unwrap();
-
-            // Send a default synthetic response.
-            Ok(Response::from_status(StatusCode::OK)
-                .with_content_type(mime::TEXT_HTML_UTF_8)
-                .with_body("Hello, world!"))
-        }
-
-        // Catch all other requests and return a 404.
-        _ => Ok(Response::from_status(StatusCode::NOT_FOUND)
-            .with_body_text_plain("The page you requested could not be found\n")),
-    }
+    // Forward all requests to the backend with cache override set to pass
+    Ok(req.with_pass(true).send("vcl-origin")?)
 }
